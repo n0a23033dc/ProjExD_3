9f368ba5dc7d21f24f4292997fcfbd9025200329
Kinjo Okuno <c0a23033dc@edu.teu.ac.jp>
2024-12-03 14:26:37 +0900

    練習2:2爆弾とビームの衝突判定

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -159,16 +159,24 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
 
+        if beam is not None:
+            if beam.rct.colliderect(bomb.rct): # 爆弾とビームの衝突判定
+                bomb = None
+                beam = None
+        
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        bomb.update(screen)
+
+        if bomb is not None:
+            bomb.update(screen)
         if beam is not None:
             beam.update(screen)
         # bomb1.update(screen)
